0f1df7bdc4d29040f0893c26a2dc78adaee4b3d6
redpois0n <red@cmail.nu>
2015-06-01 16:45:55 +0200

    only prints country stats table if no error

--- index.php
+++ index.php
@@ -30,47 +30,51 @@ require_once "layout/header.php";
 			echo "<font color=#ff0000 size=32>" . $request->sock . "</font>";
 		}
 		?>
-		<div class="proton-widget general-stats">
-			<div class="panel panel-primary front">
-				<div class="panel-heading">
-					<i class="icon-sort"></i> <span>Country Stats</span>
-				</div>
-				<ul class="list-group">
+		
 
 				<?php
-				if (empty($countrystats)) {
-					echo '<li class="list-group-item"><div class="text-holder"><span class="title-text"> ';
-					echo "0";
-					echo ' </span> <span class="description-text"> ';
-					echo "None";
-					echo '</span></div> <span class="stat-value"> ';
-					echo " + 0 ";
-					echo '<i class="icon-sort"></i></span>';
-				} else {
-					arsort($countryStats);
-					$numberValues = array_values($countryStats);
-					for($i = 0; $i < 5 && $i < count($countryStats); $i ++) {
-						$number = $numberValues [$i];
-						$short = key($countryStats);
-						next($countryStats);
-						
-						$icon = "<img src='images/flags/" . strtolower($short) . ".png'>";
-						$displayCountry = Countries::short2long($short);
-						
+				if (! $request->isError()) {
+					echo '<div class="proton-widget general-stats">
+					<div class="panel panel-primary front">
+						<div class="panel-heading">
+							<i class="icon-sort"></i> <span>Country Stats</span>
+						</div>
+						<ul class="list-group">';
+					
+					if (empty($countrystats)) {
 						echo '<li class="list-group-item"><div class="text-holder"><span class="title-text"> ';
-						echo $number;
+						echo "0";
 						echo ' </span> <span class="description-text"> ';
-						echo $icon . " " . $displayCountry;
+						echo "None";
 						echo '</span></div> <span class="stat-value"> ';
 						echo " + 0 ";
 						echo '<i class="icon-sort"></i></span>';
+					} else {
+						arsort($countryStats);
+						$numberValues = array_values($countryStats);
+						for($i = 0; $i < 5 && $i < count($countryStats); $i ++) {
+							$number = $numberValues [$i];
+							$short = key($countryStats);
+							next($countryStats);
+							
+							$icon = "<img src='images/flags/" . strtolower($short) . ".png'>";
+							$displayCountry = Countries::short2long($short);
+							
+							echo '<li class="list-group-item"><div class="text-holder"><span class="title-text"> ';
+							echo $number;
+							echo ' </span> <span class="description-text"> ';
+							echo $icon . " " . $displayCountry;
+							echo '</span></div> <span class="stat-value"> ';
+							echo " + 0 ";
+							echo '<i class="icon-sort"></i></span>';
+						}
+						
+						echo '</ul>
+						</div>
+						</div>';
 					}
 				}
 				?>
-							
-				</ul>
-			</div>
-		</div>
 
 				<?php
 				if (! $request->isError()) {
